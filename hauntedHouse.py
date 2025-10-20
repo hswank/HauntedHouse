6723911b831c9b8c86e01b034c211202276ca908
Heather Swank <hdamoth@gmail.com>
2025-10-19 21:24:23 -0400

    Added intro and repeat play functions

--- hauntedHouse.py
+++ hauntedHouse.py
@@ -4,5 +4,25 @@ import random
 def displayIntro():
     print('''Welcome to the Monster Mansion, where danger lurks around
 every corner and while everyone is allowed in, not everyone who enters
-makes it to the exit. Would you like to test your courage by walking in?''')
+makes it to the exit. Would you like to test your courage?''')
     
+def chooseEntrance():
+    entrance = ''
+    while entrance != 'f' and entrance != 's':
+        print('Would you like to go in the front door (f) or side door (s)?')
+        entrance = input()
+
+    if entrance == 'f':
+        entrance = 1
+    else:
+        entrance = 2
+
+    return entrance
+
+
+playAgain = 'yes'
+while playAgain == 'yes' or playAgain == 'y':
+    displayIntro()
+
+    print('Do you want to play again? (yes or no)')
+    playAgain = input()
