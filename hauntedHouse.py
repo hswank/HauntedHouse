8eb18c2845cd0b7c9bec68fc5593b8e50112fe96
Heather Swank <hdamoth@gmail.com>
2025-10-19 21:15:19 -0400

    Creating the python file

---
+++ hauntedHouse.py
@@ -0,0 +1,8 @@
+import time
+import random
+
+def displayIntro():
+    print('''Welcome to the Monster Mansion, where danger lurks around
+every corner and while everyone is allowed in, not everyone who enters
+makes it to the exit. Would you like to test your courage by walking in?''')
+    
